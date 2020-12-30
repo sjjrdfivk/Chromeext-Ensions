f1822efbfaddebe7dcece65705a1bd0a68965592
sjjrdfivk <401810445@qq.com>
2020-12-30 01:43:52 -0800

    响应拦截

--- js/background.js
+++ js/background.js
@@ -1,3 +1,11 @@
+const app = {};
+
+app.webRequest = {
+	"listener": function () {
+		chrome.webRequest.onHeadersReceived.removeListener();
+	}
+}
+
 chrome.contextMenus.create({
 	title: '使用百度搜索：%s', // %s表示选中的文字
   contexts: ['selection'], // 只有当选中文字时才会出现此右键菜单
@@ -7,6 +15,14 @@ chrome.contextMenus.create({
 		chrome.tabs.create({url: 'https://www.baidu.com/s?ie=utf-8&wd=' + encodeURI(params.selectionText)});
 	}
 });
+
+// 跨域
+// chrome.webRequest.onHeadersReceived.addListener(function (info) {
+// 	// const responseHeaders = info.responseHeaders.filter(e => e.name.toLowerCase() !== "access-control-allow-origin" && e.name.toLowerCase() !== "access-control-allow-methods");
+// 	const responseHeaders = [...info.responseHeaders];
+// 	responseHeaders.push({"name": "Access-Control-Allow-Origin", "value": '*'})
+// 	return {"responseHeaders": responseHeaders};
+// }, {"urls": ["http://*/*", "https://*/*"]}, ["blocking", "responseHeaders", "extraHeaders"]);
 
 chrome.contextMenus.create({
 	title: 'JSON解析：%s', // %s表示选中的文字
@@ -19,7 +35,7 @@ chrome.contextMenus.create({
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs)
 	{
 		console.log(tabs)
-		chrome.windows.create({url:chrome.extension.getURL('demo_json.html'),tabId: tabs.id},)
+		chrome.windows.create({url:chrome.extension.getURL('demo_json.html'),tabId: tabs.id, type:['popup']},)
 	});
 	}
 });
